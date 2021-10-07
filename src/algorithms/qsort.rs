55b4dbe00593dd1afca92ef4696651a37fd86a8f
Narciso Benigno <narciso.benigno@indebted.co>
2021-10-08 01:09:50 +1100

    refactor: optimize qsort to pick a random pivot

--- src/algorithms/qsort.rs
+++ src/algorithms/qsort.rs
@@ -1,13 +1,21 @@
+use rand::Rng;
 
 fn qsort(elements: &[i32]) -> Vec<i32> {
     if elements.is_empty() {
         return vec![];
     }
 
-    let pivot = elements[0];
-    let tail = &elements[1..];
-    let smaller: Vec<i32> = tail.iter().cloned().filter(|x| *x <= pivot).collect();
-    let bigger: Vec<i32> = tail.iter().cloned().filter(|x| *x > pivot).collect();
+    let pivot_i = rand::thread_rng().gen_range(0..elements.len());
+    let pivot = elements[pivot_i];
+    let mut smaller: Vec<i32> = Vec::with_capacity(elements.len());
+    let mut bigger: Vec<i32> = Vec::with_capacity(elements.len());
+
+    for (i, e) in elements.iter().enumerate() {
+        if pivot_i == i { continue }
+
+        if *e < pivot { smaller.push(*e) }
+        else { bigger.push(*e) }
+    }
 
     [
         qsort(&smaller[0..]),
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate rand;
+
 use functional::Function;
 
 mod functional;
